2d819553ecae7cc50cbc3054a23f29976ba1f4f3
Krisan Alfa Timur <krisan47@gmail.com>
2013-06-24 21:00:39 +0700

    Fixing and linted for Angel works

--- participants/AngelaCrisanti/python.py
+++ participants/AngelaCrisanti/python.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 
 
 def arrayOperation(array, operation):
@@ -13,7 +14,6 @@ def arrayOperation(array, operation):
         return a
     if(operation == "avg"):
         return float(a) / len(array)
-        print arrayOperation(myArray, "avg")
     if(operation == "mul"):
         a = int(array[0])
         for x in xrange(1, len(array)):
@@ -23,20 +23,41 @@ def arrayOperation(array, operation):
             else:
                 a = a * array[x]
         return a
-        print arrayOperation(myArray, "mul")
+        # print arrayOperation(array, "mul")
     else:
         return "Invalid operation!"
 
-print "Berapa angka yang mau dioperasikan?"
-x = int(raw_input())
-number=[]
-for x in xrange(0, x):
-    number.append(int(raw_input("Angkanya : ")))
-  
-operasi = str(raw_input("Operasi yang akan dijalankan :"))
-if (operasi == "sum"):
+# print "Berapa angka yang mau dioperasikan?"
+# x = int(raw_input())
+number = []
+# for x in xrange(0, x):
+#     number.append(int(raw_input("Angkanya : ")))
+i = int(raw_input("Berapa angka yang mau dioperasikan? "))
+for x in xrange(0, i):
+    number.append(int(raw_input("[%d] Angkanya: " % x)))
+
+
+# operasi = str(raw_input("Operasi yang akan dijalankan: "))
+# print arrayOperation(number, str(raw_input("Operasi yang akan dijalankan: ")))
+# if (operasi == "sum"):
+#     print arrayOperation(number, "sum")
+# if (operasi == "avg"):
+#     print arrayOperation(number, "avg")
+# if (operasi == "mul"):
+#     print arrayOperation(number, "mul")
+
+print "1. Penjumlahan\n2. Rata-rata\n3. Perkalian"
+operasi = int(raw_input("Operasi yang akan dijalankan: "))
+
+if (operasi == 1):
     print arrayOperation(number, "sum")
-if (operasi == "avg"):
+    sys.exit(0)
+if (operasi == 2):
     print arrayOperation(number, "avg")
-if (operasi == "mul"):
+    sys.exit(0)
+if (operasi == 3):
     print arrayOperation(number, "mul")
+    sys.exit(0)
+else:
+    print arrayOperation(number, "invalid")
+    sys.exit(1)
